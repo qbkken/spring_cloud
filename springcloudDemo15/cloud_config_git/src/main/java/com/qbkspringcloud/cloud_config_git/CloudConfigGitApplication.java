539cc5464ce84a810a430cebc98ae13c61419601
qbkken <309827240@qq.com>
2018-04-10 13:32:40 +0800

    cloud_config

---
+++ springcloudDemo15/cloud_config_git/src/main/java/com/qbkspringcloud/cloud_config_git/CloudConfigGitApplication.java
@@ -0,0 +1,12 @@
+package com.qbkspringcloud.cloud_config_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigGitApplication.class, args);
+    }
+}
---
+++ springcloudDemo15/cloud_config_git/src/test/java/com/qbkspringcloud/cloud_config_git/CloudConfigGitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qbkspringcloud.cloud_config_git;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudConfigGitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
